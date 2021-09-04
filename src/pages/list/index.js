5062284e1f09ad124bc94b9d734f2cd29f456d4a
侯少平 <hsp_email@163.com>
2021-09-04 09:00:01 +0800

    init

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,54 +1,55 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {actions} from '../../constants/counter';
+import React from "react";
+import { connect } from "react-redux";
+import { actions } from "../../constants/counter";
 
-class List extends React.Component{
-	constructor(props){
-		super(props)
-		this.state = {
-            data:[]
-        }
-	}
-	
-	/*
-	*组件挂载完毕后调用 请求数据
-	*/
-	componentDidMount(){
-		let toKen= localStorage.getItem("tm-toKen");
-		if(toKen){
-			this.props.dispatch({
-				type:actions.LTST
-			})
-		}else{
-			this.props.history.push("/");
-		}
-	}
-	componentWillUpdate(nextProps,nextstate){
-		if(nextProps !== this.props){
-			if(nextProps.state ===1 ){
-				this.setState({
-					data: nextProps.data
-				})
-			}
-		}
-	}
-	
-    render(){
-        return (
-            <div>
-				商品列表:
-				{
-					this.props.data && this.props.data.map((item,index)=>{
-						return <div key={index}>1{item.name}</div>
-					})
-				}
-            </div>
-        )
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  /*
+   *组件挂载完毕后调用 请求数据
+   */
+  componentDidMount() {
+    console.log(this.props);
+    let toKen = localStorage.getItem("tm-toKen");
+    if (toKen) {
+      this.props.dispatch({
+        type: actions.LTST,
+      });
+    } else {
+      this.props.history.push("/");
     }
+  }
+  componentWillUpdate(nextProps, nextstate) {
+		console.log(nextProps)
+    if (nextProps !== this.props) {
+      if (nextProps.state === 1) {
+        this.setState({
+          data: nextProps.data,
+        });
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        商品列表:
+        {this.props.data &&
+          this.props.data.map((item, index) => {
+            return <div key={index}>1{item.name}</div>;
+          })}
+      </div>
+    );
+  }
 }
-const mapStateToProps = (state, ownProps) =>{
-	return {
-		...state.list
-	}
-}
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ...state.list,
+  };
+};
 export default connect(mapStateToProps)(List);
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,23 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from "redux";
 
-import {userReducer} from './reducers/userReducer'
+import { userReducer } from "./reducers/userReducer";
 
-import {listReducer} from './reducers/listReducer';
+import { listReducer } from "./reducers/listReducer";
 
-//关联saga 
-import createSagaMiddleware from 'redux-saga';
+//关联saga
+import createSagaMiddleware from "redux-saga";
 
-import {defSaga} from './sagas'
+import { defSaga } from "./sagas";
 
-const middware = createSagaMiddleware();//调用一下得到中间件
-
+const middware = createSagaMiddleware(); //调用一下得到中间件
 
 export default createStore(
-    combineReducers({
-        user:userReducer,
-        list:listReducer
-    }),{},applyMiddleware(middware)
+  combineReducers({
+    user: userReducer,
+    list: listReducer,
+  }),
+  {},
+  applyMiddleware(middware)
 ); //applyMiddleware吧中间件关联到store中
 
-middware.run(defSaga)
+middware.run(defSaga);
